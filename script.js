7ec177bc9602a02622814c41ccbf93794c142bc5
namrataswain456@gmail.com <namrataswain456@gmail.com>
2021-01-10 22:26:06 +0530

    loading from local storage working

--- script.js
+++ script.js
@@ -183,17 +183,20 @@ saveStorageBtn.addEventListener('click', () => {
   setTimeout(switchToBrush, 1500);
 });
 
-// // Load from Local Storage
-// loadStorageBtn.addEventListener('click', () => {
-//   if (localStorage.) {
-//     drawnArray = JSON(localStorage.);
+// Load from Local Storage
+loadStorageBtn.addEventListener('click', () => {
+  if (localStorage.getItem('savedCanvas')) {
+    drawnArray = JSON.parse(localStorage.savedCanvas);
+     restoreCanvas();
+  // Active Tool
+    activeToolEl.textContent = 'Canvas Loaded';
+    setTimeout(switchToBrush, 1500);
+  } 
+  else{
+      activeToolEl.textContent = 'No canvas found';
+  }
 
-//   // Active Tool
-//     activeToolEl.textContent = 'Canvas Loaded';
-//     setTimeout(switchToBrush, 1500);
-//   } 
-
-// });
+});
 
 // // Clear Local Storage
 // clearStorageBtn.addEventListener('click', () => {
